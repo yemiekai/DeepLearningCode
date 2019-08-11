cbbd821d444abfa98b3ccae5115f56685645f91f
yemiekai <418504286@qq.com>
2019-08-11 13:37:32 +0800

    complete first version of training mobilenetV3

--- in_tensorflow/train/train_mobileNetV3_arcFace.py
+++ in_tensorflow/train/train_mobileNetV3_arcFace.py
@@ -11,8 +11,9 @@ from models.mobilenet_v3 import *
 def get_parser():
     parser = argparse.ArgumentParser(description='parameters to train net')
     parser.add_argument('--train_datasets_dir', default=r'F:\DeepLearning_DataSet\VGGFace2_train_mtcnnpy_224_tfrecord', help='train datasets base path')
+    parser.add_argument('--gpu', default=1, help='gpu nums')
     parser.add_argument('--batch_size', default=64, help='batch size to train network')
-    parser.add_argument('--buffer_size', default=12800, help='tf dataset api buffer size')
+    parser.add_argument('--buffer_size', default=128000, help='tf dataset api buffer size')
     parser.add_argument('--num_classes', default=8631, help='classes')
     parser.add_argument('--embedding', default=512, help='classes')
 
@@ -23,9 +24,8 @@ def get_parser():
     # parser.add_argument('--weight_deacy', default=5e-4, help='learning alg momentum')
     # parser.add_argument('--eval_datasets', default=['lfw'], help='evluation datasets')
 
-    parser.add_argument('--num_classes', default=85164, help='the image size')
     parser.add_argument('--log_file_path', default=r'E:\TrainingCache\mobileNetV3_arcFace_VGGFace_tensorflow', help='the ckpt file save path')
-    # parser.add_argument('--saver_maxkeep', default=100, help='tf.train.Saver max keep ckpt files')
+    parser.add_argument('--saver_maxkeep', default=100, help='tf.train.Saver max keep ckpt files')
     parser.add_argument('--log_device_mapping', default=False, help='show device placement log')
     parser.add_argument('--summary_interval', default=300, help='interval to save summary')
     parser.add_argument('--ckpt_interval', default=1000, help='intervals to save ckpt file')
@@ -33,6 +33,44 @@ def get_parser():
     parser.add_argument('--show_info_interval', default=50, help='intervals to save ckpt file')
     args = parser.parse_args()
     return args
+
+
+def average_gradients(tower_grads):
+    """Calculate the average gradient for each shared variable across all towers.
+
+    Note that this function provides a synchronization point across all towers.
+
+    Args:
+    tower_grads: List of lists of (gradient, variable) tuples. The outer list
+      is over individual gradients. The inner list is over the gradient
+      calculation for each tower.
+    Returns:
+     List of pairs of (gradient, variable) where the gradient has been averaged
+     across all towers.
+    """
+    average_grads = []
+    for grad_and_vars in zip(*tower_grads):
+        # Note that each grad_and_vars looks like the following:
+        #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
+        grads = []
+        for g, _ in grad_and_vars:
+            # Add 0 dimension to the gradients to represent the tower.
+            expanded_g = tf.expand_dims(g, 0)
+
+            # Append on a 'tower' dimension which we will average over below.
+            grads.append(expanded_g)
+
+        # Average over the 'tower' dimension.
+        grad = tf.concat(axis=0, values=grads)
+        grad = tf.reduce_mean(grad, 0)
+
+        # Keep in mind that the Variables are redundant because they are shared
+        # across towers. So .. we will just return the first tower's pointer to
+        # the Variable.
+        v = grad_and_vars[0][1]
+        grad_and_var = (grad, v)
+        average_grads.append(grad_and_var)
+    return average_grads
 
 
 if __name__ == '__main__':
@@ -78,33 +116,48 @@ if __name__ == '__main__':
     #     ver_list.append(data_set)
     #     ver_name_list.append(db)
 
-    # 3. define network, loss, optimize method, learning rate schedule, summary writer, saver
-    # 3.1 inference phase
-    w_init_method = tf.contrib.layers.xavier_initializer(uniform=False)
-    model_out, end_points = mobilenet_v3_small(images, args.embedding, multiplier=1.0, is_training=True, reuse=None)
-
-    # 3.2 get arcface loss
-    arcface_logit = arcface_loss(embedding=model_out, labels=labels, w_init=w_init_method, out_num=args.num_classes)
-
-    # 3.3 define the cross entropy
-    inference_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=arcface_logit, labels=labels))
-
-    # 3.6 define the learning rate schedule
     lr = tf.train.piecewise_constant(global_step, boundaries=args.lr_boundaries, values=args.lr_values, name='lr_schedule')
-
-    # 3.7 define the optimize method
     opt = tf.train.MomentumOptimizer(learning_rate=lr, momentum=args.momentum)
 
-    # 3.8 get train op
-    grads = opt.compute_gradients(inference_loss)
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-        train_op = opt.apply_gradients(grads, global_step=global_step)
-    # train_op = opt.minimize(inference_loss, global_step=global_step)
+    # Calculate the gradients for each model tower.
+    tower_grads = []
+    with tf.variable_scope(tf.get_variable_scope()):
+        for i in range(args.gpu):
+            with tf.device('/gpu:%d' % i):
+                with tf.name_scope('mobileNetV3_tower_%d' % i) as scope:
+                    w_init_method = tf.contrib.layers.xavier_initializer(uniform=False)
+                    model_out, end_points = mobilenet_v3_small(images, args.embedding, multiplier=1.0, is_training=True,
+                                                               reuse=None)
+                    arcface_logit = arcface_loss(embedding=model_out, labels=labels, w_init=w_init_method,
+                                                 out_num=args.num_classes)
+                    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=arcface_logit, labels=labels,
+                                                                                   name='cross_entropy_per_example')
+                    cross_entropy_mean = tf.reduce_mean(cross_entropy, name='cross_entropy')
 
-    # 3.9 define the inference accuracy used during validate or test
-    pred = tf.nn.softmax(arcface_logit)
-    acc = tf.reduce_mean(tf.cast(tf.equal(tf.argmax(pred, axis=1), labels), dtype=tf.float32))
+                    tf.add_to_collection('losses', cross_entropy_mean)
+
+                    losses = tf.get_collection('losses', scope)
+                    total_loss = tf.add_n(losses, name='total_loss')
+
+                    # Reuse variables for the next tower.
+                    tf.get_variable_scope().reuse_variables()
+
+                    # Retain the summaries from the final tower.
+                    summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
+
+                    # Calculate the gradients for the batch of data on this CIFAR tower.
+                    grads = opt.compute_gradients(total_loss)
+
+                    # Keep track of the gradients across all towers.
+                    tower_grads.append(grads)
+
+    # We must calculate the mean of each gradient. Note that this is the
+    # synchronization point across all towers.
+    grads = average_gradients(tower_grads)
+
+    # Apply the gradients to adjust the shared variables.
+    train_op = opt.apply_gradients(grads, global_step=global_step)
+
 
     # 3.10 define sess
     config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=args.log_device_mapping)
@@ -122,8 +175,6 @@ if __name__ == '__main__':
     # 3.11.2 add trainabel variable gradients
     for var in tf.trainable_variables():
         summaries.append(tf.summary.histogram(var.op.name, var))
-    # 3.11.3 add loss summary
-    summaries.append(tf.summary.scalar('inference_loss', inference_loss))
     # 3.11.4 add learning rate
     summaries.append(tf.summary.scalar('leraning_rate', lr))
     summary_op = tf.summary.merge(summaries)
@@ -151,14 +202,14 @@ if __name__ == '__main__':
                 feed_dict = {images: images_train, labels: labels_train, dropout_rate: 0.4}
 
                 start = time.time()
-                _, inference_loss_val, _, acc_val = sess.run([train_op, inference_loss, inc_op, acc], feed_dict=feed_dict)
+                _, _ = sess.run([train_op, inc_op], feed_dict=feed_dict)
                 end = time.time()
                 pre_sec = args.batch_size/(end - start)
 
                 # print training information
                 if count > 0 and count % args.show_info_interval == 0:
-                    print('epoch %d, total_step %d, inference loss is %.2f, training accuracy is %.6f, time %.3f samples/sec' %
-                          (i, count, inference_loss_val, acc_val, pre_sec))
+                    print('epoch %d, total_step %d, time %.3f samples/sec' %
+                          (i, count, pre_sec))
                 count += 1
 
                 # save summary
